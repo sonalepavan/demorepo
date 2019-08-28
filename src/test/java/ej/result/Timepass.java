d9cd85d2baffe8db6c3c80d2bf5ce5f9c6eb7e57
unknown <sonalepavan@example.com>
2019-08-28 15:17:58 +0530

    commit for the new timepass class

---
+++ src/test/java/ej/result/Timepass.java
@@ -0,0 +1,15 @@
+package ej.result;
+
+public class Timepass 
+{
+
+	public static void printit()
+	{
+		System.out.println("Timepass method..delete it later");
+	}
+	public static void justmethod()
+	{
+		System.out.println("delete it..none of use...");
+	}
+
+}
